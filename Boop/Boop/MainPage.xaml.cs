433d5689cca33954163a1f0ad07a929c7f4cf936
Erik Nelson <nelser05@luther.edu>
2017-12-01 13:19:20 -0600

    Add project files.

---
+++ Boop/Boop/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Boop
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            Button button = new Button
+            {
+                Text = "Get Started Menu!",
+                HorizontalOptions = LayoutOptions.Center,
+                VerticalOptions = LayoutOptions.Center
+            };
+
+            button.Clicked += async (sender, args) =>
+            {
+                await Navigation.PushAsync(new MenuPage());
+            };
+
+            Content = button;
+        }
+    }
+}
